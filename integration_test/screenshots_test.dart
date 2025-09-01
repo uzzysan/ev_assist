b8979c56d60c873266168088ed1f2dd81d1dc973
Rafał Maculewicz <rafal.maculewicz@hotmail.com>
2025-09-01 14:23:51 +0200

    Comments out unused import for cleanup
    Removes an unused package import to reduce clutter and avoid potential warnings.

--- integration_test/screenshots_test.dart
+++ integration_test/screenshots_test.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
